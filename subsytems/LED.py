eb117e18cc40ea1859cd989ad548ea267b62ac7e
ThomasPinto2134 <145403603+ThomasPinto2134@users.noreply.github.com>
2024-02-24 13:06:20 -0500

    ...

---
+++ subsytems/LED.py
@@ -0,0 +1,33 @@
+import math
+
+import commands2
+import wpilib
+import wpilib.drive
+import rev
+
+class LED(commands2.SubsystemBase):
+  
+  def __init__(self) -> None:
+    super().__init__()
+    
+    self.blinkin = wpilib.Spark(0) # Change later
+    
+  # // COLOR FUNCTION LIST \\ #
+  
+  '''Value list: https://www.revrobotics.com/content/docs/REV-11-1105-UM.pdf'''
+  
+  def setBlackLed(self): # Off
+    self.blinkin.set(0.99)
+    print('Black')
+  
+  def setRedLed(self): 
+    self.blinkin.set(0.61)
+    print('Red')
+  
+  def setYellowLed(self):
+    self.blinkin.set(0.69)
+    print('Yellow')
+  
+  def setOrangeLed(self):
+    self.blinkin.set(0.65)
+    print('Orange')
